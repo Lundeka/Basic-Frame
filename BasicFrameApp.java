de3cb4a053c514940a58a5be8b21f48a406b64e0
Lundeka <139706270+Lundeka@users.noreply.github.com>
2023-07-16 10:22:52 -0700

    Add files via upload

---
+++ BasicFrameApp.java
@@ -0,0 +1,28 @@
+//FRONTEND CLASS
+//add JAR file to libraries n import
+package basicframeapp;
+
+import za.ac.tut.ui.MyFirstFrame;
+
+/**
+ *
+ * @author User
+ */
+public class BasicFrameApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //declare a BACKEND frame
+        MyFirstFrame mff;
+        
+        //instantiate the frame
+        mff = new MyFirstFrame();
+        
+        
+        
+    }
+    
+}
